75a2d843dba2f05f2980f9e9aa1671f0ca4cbf1d
Swordzi <github@sword.mozmail.com>
2022-06-07 12:48:24 +0300

    Tests, HomePage

---
+++ vulzedorg/web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,24 @@
+import { Link, routes } from '@redwoodjs/router'
+import { MetaTags } from '@redwoodjs/web'
+import { Button } from '@mantine/core';
+
+import './HomePage.scss'
+
+const HomePage = () => {
+  return (
+    <div className='Home'>
+      <MetaTags title="Home" description="Home page" />
+      <Button className='Hello'>Hello there</Button>
+      <h1>HomePage</h1>
+      <p>
+        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
+      </p>
+      <p>
+        My default route is named <code>home</code>, link to me with `
+        <Link to={routes.home()}>Home</Link>`
+      </p>
+    </div>
+  )
+}
+
+export default HomePage
